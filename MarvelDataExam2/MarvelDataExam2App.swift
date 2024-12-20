a7f3175f1a164a8723468ed4a34263720afb7972
Pablo Arias <pabloarias.ic@gmail.com>
2024-12-20 09:27:57 -0300

    Initial Commit

---
+++ MarvelDataExam2/MarvelDataExam2App.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelDataExam2App.swift
+//  MarvelDataExam2
+//
+//  Created by Pablo on 20-12-24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelDataExam2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
